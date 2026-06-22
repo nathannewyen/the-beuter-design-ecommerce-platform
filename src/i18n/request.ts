4f08256d1b1f0f62a4907e632d24874d0c87743c
nathannewyen <nhan13574@gmail.com>
2026-06-22 11:46:00 -0400

    feat(i18n): scaffold next-intl with cookie-based EN/VN locale and message catalogs

---
+++ src/i18n/request.ts
@@ -0,0 +1,11 @@
+import { cookies } from "next/headers";
+import { getRequestConfig } from "next-intl/server";
+import { DEFAULT_LOCALE, LOCALE_COOKIE, isLocale } from "./config";
+
+export default getRequestConfig(async () => {
+  const cookieStore = await cookies();
+  const cookieLocale = cookieStore.get(LOCALE_COOKIE)?.value;
+  const locale = isLocale(cookieLocale) ? cookieLocale : DEFAULT_LOCALE;
+  const messages = (await import(`./messages/${locale}.json`)).default;
+  return { locale, messages };
+});
